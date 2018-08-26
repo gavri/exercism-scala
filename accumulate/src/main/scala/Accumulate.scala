b54bb4438d7a5d9b0877d1446a0a874ddc253e2c
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-26 06:44:54 +0530

    Accumulate: with fewer mutable data structures

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,11 +1,10 @@
 import scala.annotation.tailrec
-import scala.collection.mutable.ListBuffer
 
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = accumulateRecur(f, list, new ListBuffer[B]()).toList
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = accumulateRecur(f, list, List[B]()).reverse
 
-  @tailrec private[this] def accumulateRecur[A, B](f: (A) => B, list: List[A], resultSoFar: ListBuffer[B]): ListBuffer[B] = list match {
+  @tailrec private[this] def accumulateRecur[A, B](f: (A) => B, list: List[A], resultSoFar: List[B]): List[B] = list match {
     case Nil => resultSoFar
-    case x :: xs => accumulateRecur(f, xs, resultSoFar += f(x))
+    case x :: xs => accumulateRecur(f, xs, f(x) :: resultSoFar)
   }
 }
