d8f9fe47b452cc5ce8e1e34421549c5c0aa63ed2
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-30 07:50:30 +0530

    ETL: for comprehension

--- etl/src/main/scala/Etl.scala
+++ etl/src/main/scala/Etl.scala
@@ -2,8 +2,9 @@ object Etl {
   def transform(input: Map[Int, Seq[String]]): Map[String, Int] = transformList(input.toList).toMap
 
   private[this] def transformList(input: List[(Int, Seq[String])]): List[(String, Int)] = {
-    input.flatMap { case(score, letters) =>
-      letters.map {(letter: String) => (letter.toLowerCase, score)}
-    }
+    for {
+      (score, letters) <- input
+      letter <- letters
+    } yield (letter.toLowerCase, score)
   }
 }
